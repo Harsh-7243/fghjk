d5fd74c4c489c1651f92362ae36fa879033c677f
Harsh-7243 <srivastavaharsh1108@gmail.com>
2025-05-23 10:37:11 +0530

    Update App.js
    adding alert on login

--- src/App.js
+++ src/App.js
@@ -173,7 +173,9 @@ const Header = () => {
           >
             Store
           </button>
-          <button className="px-4 py-1 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
+          <button
+              onClick={() => handleClick("Store")} 
+              className="px-4 py-1 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
             Login
           </button>
         </nav>
